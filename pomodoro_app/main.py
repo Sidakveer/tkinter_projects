023d4268f9d6be389bcf174d817cd384bd7c08ff
Sidakveer <sidakveersingh8@gmail.com>
2022-04-30 14:30:52 -0400

    ui setup

--- pomodoro_app/main.py
+++ pomodoro_app/main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,44 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
+mainWindow = Tk()
+mainWindow.title("Pomodoro")
+mainWindow.config(padx=100, pady=100, bg=YELLOW)
+
+
+canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 111, image=tomato_img)
+canvas.create_text(103, 130, text="00:00", fill="White", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+timer_label = Label(text="TIMER", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
+timer_label.grid(row=0, column=1)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(row=2, column=2)
+
+tick_label = Label(text="✔", fg=GREEN, bg=YELLOW)
+tick_label.grid(row=3, column=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mainWindow.mainloop()

dd187d4daf23bbfa8e2f510e7883724bb156c17d
Sidakveer <sidakveersingh8@gmail.com>
2022-04-30 13:36:02 -0400

    finish code

--- Mile_to_Kilometers/main.py
+++ Mile_to_Kilometers/main.py
@@ -1,27 +1,37 @@
 from cgitb import text
 from tkinter import *
 
+def update_ans():
+    x = input.get()
+    km = float(x) * 1.609
+    label_ans.config(text=km)
+
+
 mainWindow = Tk()
-mainWindow.title(text="Mile to Km Converter")
+mainWindow.title("Mile to Km Converter")
+mainWindow.config(padx=20, pady=20)
+
+input = Entry(width=10)
+input.grid(row=0, column=1)
+input.get()
+
 
 label = Label(text="Miles")
 label.grid(row=0, column=2)
 
 label1 = Label(text="is equal to ")
-label1.grid(row=1, column=0, sticky="nsew")
+label1.grid(row=1, column=0,)
 
 
-label_ans = Label(text=input.get())
+label_ans = Label(text="0.0", )
 label_ans.grid(row=1, column=1, sticky="nsew")
-
 
 label2 = Label(text="Km")
 label2.grid(row=1, column=2, sticky="nsew")
 
-button = Button(text="Calculate")
+button = Button(text="Calculate", command=update_ans)
 button.grid(row=2, column=1)
 
-input = Entry()
-input.grid(row=0, column=1)
+
 
 mainWindow.mainloop()
